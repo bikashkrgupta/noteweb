2bb40fe230a494ba6a36478b2fcb9213dbd1020b
bikashkrgupta <43280465+bikashkrgupta@users.noreply.github.com>
2018-09-19 21:48:43 +0530

    Update form.py

--- form.py
+++ form.py
@@ -6,8 +6,6 @@ app.config["SESSION_PERMANENT"]=False
 app.config["SESSION_TYPE"]="filesystem"
 Session(app)
 
-notes=[]
-
 @app.route("/",methods=["GET","POST"])
 def index():
     if session.get("notes") is None:
